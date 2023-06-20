6c3a4f21a2bbbd0fae0224f8202672782710a80b
ItsPinkuuu <102356167+ItsPinkuuu@users.noreply.github.com>
2023-06-20 11:45:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,121 @@
-let main__img = document.querySelectorAll(".main__img");
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
 
 
-function randomSize(min,max) {
-    min = Math.ceil(40);
-    max = Math.ceil(150);
-    return Math.floor(Math.random() * (max - min)) + min;
+const mobileMenu = () => {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+}
+
+menu.addEventListener("click", mobileMenu);
+
+
+
+
+
+const highlightMenu = () => {
+    const elem = document.querySelector(".highlight");
+    const homeMenu = document.querySelector("#home-page");
+    const aboutMenu = document.querySelector("#about-page");
+    const linksMenu = document.querySelector("#links-page");
+    let scrollPos = window.scrollY;
+    
+
+
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add("highlight");
+        aboutMenu.classList.remove("highlight");
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add("highlight");
+        homeMenu.classList.remove("highlight");
+        linksMenu.classList.remove("highlight");
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        linksMenu.classList.add("highlight");
+        aboutMenu.classList.remove("highlight");
+        return;
+    }
+
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove("highlight");
+    }
+
+
+};
+
+
+window.addEventListener("scroll", highlightMenu);
+
+window.addEventListener("click", highlightMenu);
+
+
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector(".is-active")
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle("is-active")
+        menuLinks.classList.remove("active")
+    }
 }
 
 
+menuLinks.addEventListener("click", hideMobileMenu)
+navLogo.addEventListener("click", hideMobileMenu)
 
 
 
-function rotatePointers(){
-    main__img.forEach(function(main__img){
-        let x = (main__img.getBoundingClientRect().left) + (main__img.clientWidth / 2);
-        let y = (main__img.getBoundingClientRect().top) + (main__img.clientHeight / 2);
 
-        let radian = Math.atan2(event.pageX - x, event.pageY - y);
-        let rot = (radian * (180 / Math.PI) * -1) + 270;
-        main__img.style.transform = "rotate(" + rot + "deg)";
+
+
+
+
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+const handleRightClick = () => {
+    // Bump active index
+
+    const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex +1 : 0;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    // Active group becomes after
+
+    currentGroup.dataset.status = "after";
+
+    // Next group becomes active
+
+    nextGroup.dataset.status = "becoming-active-from-before";
+
+    setTimeout(() => {
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
     })
+
+
 }
 
-document.querySelector("body").addEventListener("mousemove",rotatePointers);
+const handleLeftClick = () => {
+    
+
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "before";
+
+    
+
+    nextGroup.dataset.status = "becoming-active-from-after";
+
+    setTimeout(() => {
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    })
+
+}
